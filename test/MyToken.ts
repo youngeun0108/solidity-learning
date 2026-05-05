5ff177a80ea0f2176a8eb365d47de3b035462f44
github youngeun0108 <duddmsakxm@naver.com>
2026-05-05 20:10:45 +0900

    add malicious minting try test

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -40,7 +40,17 @@ describe("My Token", () => {
         MINTING_AMOUNT * 10n ** DECIMALS,
       );
     });
+
+    it("should return or revert when minting infinitly", async () => {
+      const hacker = signers[2];
+      const mintingAgainAmount = hre.ethers.parseUnits("10000", DECIMALS);
+
+      await expect(
+        myTokenC.connect(hacker).mint(mintingAgainAmount, hacker.address),
+      ).to.be.revertedWith("You are not authorized to manage this token");
+    });
   });
+
   describe("Transfer", () => {
     it("shoud have 0.5MT", async () => {
       const signer0 = signers[0];
